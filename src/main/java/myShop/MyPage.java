36fc2ff7181f6c277a7d30ac0bac66c46b866cee
wooktae1 <yukiever@naver.com>
2020-09-16 10:58:19 +0900

    first

---
+++ src/main/java/myShop/MyPage.java
@@ -0,0 +1,55 @@
+package myShop;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="MyPage_table")
+public class MyPage {
+
+        @Id
+        @GeneratedValue(strategy=GenerationType.AUTO)
+        private Long id;
+        private Long orderId;
+        private String productId;
+        private Integer qty;
+        private String status;
+
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+        public Long getOrderId() {
+            return orderId;
+        }
+
+        public void setOrderId(Long orderId) {
+            this.orderId = orderId;
+        }
+        public String getProductId() {
+            return productId;
+        }
+
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+        public Integer getQty() {
+            return qty;
+        }
+
+        public void setQty(Integer qty) {
+            this.qty = qty;
+        }
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+}
---
+++ src/main/java/myShop/MyPageRepository.java
@@ -0,0 +1,13 @@
+package myShop;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MyPageRepository extends CrudRepository<MyPage, Long> {
+
+    List<MyPage> findByOrderId(Long orderId);
+
+
+}
